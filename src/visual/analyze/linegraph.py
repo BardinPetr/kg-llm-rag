129eff1520ccacba57d8d683a23412ebdf80554c
Bardin Petr <bardin.petr@gmail.com>
2026-03-17 22:40:23 +0300

    graph merging

--- src/visual/analyze/linegraph.py
+++ src/visual/analyze/linegraph.py
@@ -1,24 +1,29 @@
 import random
+from copy import copy
 from dataclasses import dataclass, field
 from enum import StrEnum
 from typing import *
 from uuid import uuid4
-from copy import copy
+
 import cv2
 import networkx as nx
+import numpy as np
 from bidict import bidict
-from shapely import LineString, Polygon, Point, STRtree
+from shapely import LineString, Polygon
+from shapely import Point, STRtree
 
-from utils.gui import to_rgb_or_copy, color_range_01
+from utils.gui import to_rgb_or_copy, color_range_01, pt_c
 from utils.utils import imap
-
-
-def pt_c(x: Point) -> Tuple[int, int]:
-    return imap(list(x.coords[0]))
+from visual.analyze.ocr.model import OCRText
+from visual.analyze.structure.node import ImageGraphNode
 
 
 def uid():
     return str(uuid4().hex)
+
+
+class BaseObj(Protocol):
+    uid: str
 
 
 @dataclass
@@ -36,6 +41,7 @@ class NType(StrEnum):
     BOUND_END = "BE"
     BOUND_INT = "BI"
     OBJECT = "OB"
+    TEXTBOX = "TE"
     ARROWHEAD = "AH"
 
     def __str__(self):
@@ -47,8 +53,8 @@ class NType(StrEnum):
 
 @dataclass
 class GNode:
-    pt: Point
-    box: Optional[Polygon] = None
+    pt: Point | Polygon
+    data: Any = None
     typ: NType = NType.UNBOUND
     uid: str = field(default_factory=uid)
 
@@ -94,6 +100,20 @@ class GEdge:
     def u_end(self) -> str:
         return self.nds[-1].uid
 
+
+def score_partial(val, v_min, v_max):
+    scaled = (val - v_min) / (v_max - v_min)
+    return scaled if v_min <= val <= v_max else np.inf
+
+
+def rank_pairs(ranker, own_seg: GEdge, others: List[GEdge], max_score=1) -> List[Tuple[float, GEdge]]:
+    variants = [(score, i)
+                for i in others
+                if (score := ranker(own_seg, i)) < max_score]
+    variants.sort(key=lambda i: i[0])
+    return variants
+
+
 @dataclass
 class LineGraph:
     g: nx.Graph
@@ -115,7 +135,20 @@ class LineGraph:
                 self.add_node(t)
                 uids.append(t.uid)
             self.add_edge(*uids, CType.HARD)
-        self.recalc()
+
+    def add_objects(self, objects: List[ImageGraphNode]):
+        for i in objects:
+            t = GNode(pt=i.box.centroid, data=i, typ=NType.OBJECT, uid=i.id)
+            self.add_node(t)
+
+    def add_texts(self, objects: List[OCRText]):
+        for i in objects:
+            t = GNode(pt=i.bbox.polygon().centroid, data=i, typ=NType.TEXTBOX)
+            self.add_node(t)
+
+    def add_arrowheads(self, objects: List[Point]):
+        for i in objects:
+            self.add_node(GNode(pt=i, typ=NType.ARROWHEAD))
 
     """"""
 
@@ -132,6 +165,9 @@ class LineGraph:
         self.mark_node(nid1, n_type)
         self.mark_node(nid2, n_type)
         self.add_edge(nid1, nid2, e_con_type)
+
+    def mark_node(self, nid, typ):
+        self.node(nid).typ = typ
 
     """"""
 
@@ -178,7 +214,7 @@ class LineGraph:
         nds = self.nodes([nid1, nid2])
         return GEdge(CType.POSS, nds)
 
-    def propose_for_node(self, nid1):
+    def propose_for_node(self, nid1, search_radius=20):
         node = self.node(nid1)
         if node.typ != NType.UNBOUND: return None
         all_our_ids = [i.nds[1].uid for i in self.neighbors(nid1)] + [nid1]
@@ -187,23 +223,34 @@ class LineGraph:
         if len(our_edge_ends) > 1: return None
         our_edge = our_edge_ends[0]  # from "node" to other
 
-        # print(f"@@@@@ for {node}")
-        possible_starts = self.nearby_nodes(nid1, 20)
+        possible_starts = self.nearby_nodes(nid1, search_radius)
         possible_next_edges: List[GEdge] = []
         for ps_start in possible_starts:
             if ps_start.typ != NType.UNBOUND: continue
             if ps_start.uid in all_our_ids: continue
-            # print(ps_start)
             ps_edges = self.neighbors(ps_start.uid, edge_typ=CType.HARD)
-            ps_edges = [i for i in ps_edges if i.nds[0].typ == NType.UNBOUND and i.nds[1].typ == NType.UNBOUND]
+            ps_edges = [i for i in ps_edges if i.nds[0].typ == NType.UNBOUND]
             if len(ps_edges) != 1: continue
             possible_next_edges.append(ps_edges[0])
 
         if not possible_next_edges: return None
         return our_edge, possible_next_edges
 
-    def mark_node(self, nid, typ):
-        self.node(nid).typ = typ
+    def connect_softly(self, scorer: Callable[[GEdge, GEdge], float], max_score=1, search_radius=20):
+        for node in self.unbound_nodes():
+            if node.typ != NType.UNBOUND: continue
+            if (proposal := self.propose_for_node(node.uid, search_radius)) is None: continue
+            cur_edge, proposed_edges = proposal
+
+            variants = rank_pairs(scorer, cur_edge, proposed_edges, max_score)
+            if len(variants) == 0: continue
+
+            score, other_edge = variants[0]
+            self.connect(cur_edge.u_start, other_edge.u_start, CType.SOFT, NType.BOUND_INT)
+
+    def connect_terminals(self, scorer: Callable[[GEdge, GEdge], float], max_dist):
+        for node in self.unbound_nodes():
+            pass
 
     """"""
 
@@ -213,7 +260,14 @@ class LineGraph:
         return nodes
 
     def recalc(self):
-        n_list = self.nodes()
+        n_list = [i for i in self.nodes() if i.typ in {
+            NType.UNBOUND,
+            NType.BOUND_INT,
+            NType.BOUND_START,
+            NType.BOUND_END,
+            NType.ARROWHEAD,
+            NType.TEXTBOX
+        }]
         n_list.sort(key=lambda i: i.uid)
         self.strt_enc = bidict({n.uid: idx for idx, n in enumerate(n_list)})
         self.strt = STRtree([i.pt for i in n_list])
@@ -239,6 +293,6 @@ class LineGraph:
             col = color_range_01(random.random())
             for *nds, data in comp.edges(data='x'):
                 pa, pb = [pt_c(i.pt) for i in self.nodes(nds)]
-                cv2.line(im, pa, pb, col, 1)
+                cv2.line(im, pa, pb, col, 2)
 
         return im
