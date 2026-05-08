fa350f303d1d8cbbede19bd45edd542ee8518e2d
Bardin Petr <bardin.petr@gmail.com>
2026-05-08 10:48:00 +0300

    feat: top loader system

---
+++ src/kg/kgingest.py
@@ -0,0 +1,170 @@
+from uuid import uuid4
+
+import networkx as nx
+
+from utils.aimodel import load_llm_lc
+from ededup.ededup import EDedupService
+from utils.graphutils import g_nodes_of_type
+from kg.kgextract import EntityKG, FactKG
+from db.neo_kg import KType, KFactType, KEntity, KFact, KRelFact, KValFact
+from db.neo_doc import *
+
+
+def _names(x: Iterable[Any]) -> List[str]:
+    return [i.name for i in x]
+
+
+def _code_assoc[T](x: List[T]) -> Dict[str, T]:
+    return {i.uid: i for i in x}
+
+
+llm_small = load_llm_lc("gemini2fl")
+llm = load_llm_lc("gemini2")
+embs = EmbeddingService()
+ededup = EDedupService(embs, llm, top_k=5)
+
+
+def entity_dedup_ingest(d_block: DBlock):
+    d_block.refresh()
+    kg: nx.DiGraph = d_block.kgg.get().content
+
+    print("entity ingest start")
+
+    e_nodes = g_nodes_of_type(kg, EntityKG)
+    e_names = _names(e_nodes.values())
+    e_keys = list(e_nodes.keys())
+
+    entity_map = ededup.process(e_keys, e_names)  # new_id -> reuse_entity
+    print(f"DEDUP LOADED ENTITIES: {len(entity_map)}")
+
+    e_delta = {k: v
+               for k, v in e_nodes.items()
+               if k not in entity_map}
+
+    print(f"NEW ENTITIES: {len(e_delta)}")
+
+    print("generating types")
+    types = {i.type for i in e_nodes.values()}
+    type_map = KType.get_or_create_mapped("uid", [dict(uid=i) for i in types])
+
+    print("generating embeddings")
+    embeds = {k: e for k, e in zip(e_delta.keys(), embs.embed_all(_names(e_delta.values())))}
+
+    print("ingesting entities")
+    key_to_entity = [
+        dict(
+            uid=KEntity.hash(v.name),
+            type_code=v.type,
+            repr=v.name,
+            repr_embedding=embeds[k] # TODO
+        )
+        for k, v in e_delta.items()
+    ]
+    e_loaded = _code_assoc(KEntity.get_or_create(*key_to_entity))
+
+    for k, v in e_delta.items():
+        entity_map[v.uid] = e_loaded[KEntity.hash(v.name)]
+
+    entity_map_new2old = {new.uid: e_nodes[old] for old, new in entity_map.items()}
+
+    print("ingesting entity type & prov")
+    for e in e_loaded.values():
+        if not e.type:
+            e.type.connect(type_map[e.type_code])
+
+        original_ent = entity_map_new2old[e.uid]
+        proof = {}
+        if isinstance(d_block, DTxtBlock):
+            proof = dict(
+                loc_begin=original_ent.ref_pos,
+                loc_type="char"
+            )
+        e.mentions.connect(d_block, properties=proof)
+
+    d_block.kg_entity_map = {k: e.uid for k, e in entity_map.items()}
+    d_block.save()
+    print("done")
+
+
+def fact_ingest(d_block: DBlock):
+    print("fact ingest start")
+    d_block.refresh()
+    kg: nx.DiGraph = d_block.kgg.get().content
+    nodes = g_nodes_of_type(kg, FactKG)
+
+    print("preload entities")
+    entity_id_map: Dict[str, str] = d_block.kg_entity_map
+    entity_map: Dict[str, KEntity] = KEntity.select_mapped("uid", entity_id_map.values())
+    entity_map = {int_key: entity_map[ext_key] for int_key, ext_key in entity_id_map.items()}
+
+    print("generating types")
+    types = {i.type for i in nodes.values()}
+    type_map = KFactType.get_or_create_mapped("uid", [dict(uid=i) for i in types])
+
+    fact_map: Dict[str, KFact] = {}
+    for k, v in nodes.items():
+        params = dict(
+            uid=do_hash(uuid4().hex),
+            type_code=v.type,
+        )
+        if v.value is not None:
+            f = KValFact(
+                repr=f"{v.type}={v.value}",
+                value=str(v.value),
+                **params
+            )
+        else:
+            f = KRelFact(
+                repr=f"{v.type}",
+                **params
+            )
+
+        f.save()
+        fact_map[k] = f
+
+        proof = {}
+        if isinstance(d_block, DTxtBlock):
+            proof = dict(
+                overview=str(d_block.own_context[max(0, v.ref_pos - 100):v.ref_pos+100] + "..."),
+                loc_begin=v.ref_pos,
+                loc_type="char"
+            )
+        # TODO improve for other types
+        f.proof.connect(d_block, properties=proof)
+
+    print("ingesting fact type")
+    for e in fact_map.values():
+        if not e.type:
+            e.type.connect(type_map[e.type_code])
+
+    print("ingesting fact connections")
+    total_map = {**fact_map, **entity_map}
+    for nsi, ndi, typ in kg.edges(data="data"):
+        try:
+            src, dst = kg.nodes[nsi]['data'], kg.nodes[ndi]['data']
+        except KeyError:
+            continue
+
+        src_n, dst_n = total_map[nsi], total_map[ndi]
+        try:
+            match src, typ, dst:
+                case EntityKG(), "OWNS", FactKG():
+                    # dst is describing src;  dst is fact, src is subject
+                    dst_n.subject.connect(src_n)
+                case FactKG(), "OWNS", FactKG():
+                    # dst describes src;  dst is fact, src is subject
+                    dst_n.subject.connect(src_n)
+                case FactKG(), "POINTS", EntityKG():
+                    # dst is used to describe something via src;  src is fact, dst is object
+                    src_n.objects.connect(dst_n)
+                case _:
+                    pass
+        except Exception as ex:
+            print("fail:", src.uid, "->", dst.uid, "--", ex)
+
+    d_block.save()
+    print("done")
+
+
+def ingest_kg_provenance(d_block: DBlock):
+    pass
---
+++ src/load/top.py
@@ -0,0 +1,305 @@
+from dataclasses import dataclass, field
+from io import BytesIO
+from pathlib import Path
+
+import networkx as nx
+from docling_core.types import DoclingDocument
+from docling_core.types.io import DocumentStream
+from tqdm import tqdm
+
+from db.neo_base import n_setup
+from db.neo_doc import *
+from db.neo_kg import KType, KFactType
+from docproc.improc import docling_extract_images, describe_image, ImageCategory
+from docproc.tableproc import extract_tables
+from docproc.textproc import docling_text_only, docling_chunk
+from document.docling.provider import docling_provider, create_document_processor
+from kg.kgextract import doc_extract_kg
+from kg.kgingest import entity_dedup_ingest, fact_ingest
+
+
+@dataclass
+class DocumentFile:
+    name: str
+    content: bytes
+    metadata: Dict[str, Any] = field(default_factory=dict)
+
+    @classmethod
+    def of_file(cls, path: Path) -> 'DocumentFile':
+        return DocumentFile(
+            name=path.name,
+            content=path.read_bytes()
+        )
+
+    def hash(self) -> str:
+        return do_hash(self.content)
+
+
+def load_document(file: DocumentFile):
+    doc_file = DObject.make(file.content)
+    if doc_file.refs:
+        logger.warning(f"File {file.name} exist by content")
+        return doc_file.refs[0]
+
+    doc = DDocument(
+        name=file.name,
+        metadata=file.metadata,
+        stages=[DocumentProcStages.FILE]
+    )
+    doc.save()
+    doc.source_file.connect(doc_file)
+    return doc
+
+
+ddp = docling_provider(use_vlm=False)
+ddp = create_document_processor(ddp, "./ccache")
+
+
+def load_docling(doc: DDocument) -> DoclingDocument:
+    doc.refresh()
+    if doc.docling_file:
+        logger.info(f"Docling present for {doc.name}")
+        return doc.docling_file.get_or_none().content
+
+    try:
+        d_content = DocumentStream(name=doc.name, stream=BytesIO(doc.source_file.get().content))
+        d_doc = ddp(d_content)
+    except:
+        doc.delete()
+        return None
+
+    doc.refresh()
+    doc.stages.append(DocumentProcStages.DOCL)
+    doc.docling_file.connect(DObject.make(d_doc))
+    doc.save()
+    return d_doc
+
+
+def load_tables(doc: DDocument):
+    if DocumentProcStages.TABL in doc.stages: return
+
+    logger.info(f"Extracting tables from {doc.name}")
+    ddoc: DoclingDocument = load_docling(doc)
+
+    tables = extract_tables(ddoc)
+
+    logger.info(f"{doc.name}: detected {len(tables)} table nodes")
+    for t_descr, t_prov, t_data in tables:
+        t_block = DTblBlock(
+            title=t_descr.title,
+            own_context=t_descr.context,
+            metadata=dict(
+                transpose=not t_descr.header_top,
+                processing=t_descr.altered
+            ),
+            stages=[BlockProcStages.LOAD],
+            repr=t_descr.context
+        )
+        t_block.save()
+        t_block.content.connect(DObject.make(t_data))
+
+        loc = dict(
+            loc_page=t_prov.loc_page,
+            loc_bind=t_prov.loc_title,
+            loc_drefs=t_prov.loc_drefs,
+            loc_didx=t_prov.loc_didx,
+            loc_type="page"
+        )
+        t_block.document.connect(doc, loc)
+
+    doc.refresh()
+    doc.stages.append(DocumentProcStages.TABL)
+    doc.save()
+
+
+def load_textual(doc: DDocument):
+    if DocumentProcStages.TEXT in doc.stages: return
+
+    logger.info(f"{doc.name}: Document text base analysis")
+    ddoc: DoclingDocument = load_docling(doc)
+    doc.refresh()
+
+    summary, ddoc_text = docling_text_only(ddoc)
+    ddoc_md = ddoc_text.export_to_markdown()
+
+    t_block = DTxtBlock(
+        title=summary.title,
+        own_context=ddoc_md,
+        metadata=dict(context=summary.context, topic=summary.topic, tags=summary.tags),
+        stages=[BlockProcStages.LOAD],
+        repr=summary.context,
+    )
+    t_block.save()
+    t_block.content.connect(DObject.make(ddoc_md))
+    t_block.document.connect(doc)
+
+    for chk in docling_chunk(ddoc_text):
+        t_chunk = DChunk(repr=chk.text)
+        t_chunk.save()
+        loc = dict(
+            loc_page=chk.loc_page,
+            loc_drefs=chk.loc_drefs,
+            loc_type="page"
+        )
+        t_chunk.text_block.connect(t_block, properties=loc)
+
+    doc.refresh()
+    doc.repr = summary.context
+    doc.stages.append(DocumentProcStages.TEXT)
+    doc.save()
+
+
+def load_visual(doc: DDocument):
+    if DocumentProcStages.IMAG in doc.stages: return
+
+    logger.info(f"{doc.name}: Document image analysis")
+    ddoc: DoclingDocument = load_docling(doc)
+
+    im_data = docling_extract_images(ddoc)
+    im_descr = [describe_image(i.data) for i in im_data]
+
+    logger.info(f"{doc.name}: detected {len(im_data)} image nodes")
+
+    for dat, dsc in zip(im_data, im_descr):
+        if not dsc: continue
+        if dsc.category == ImageCategory.DROP: continue
+        t_block = DImgBlock(
+            title=dsc.title,
+            own_context=dsc.content,
+            metadata=dict(
+                preview=dsc.preview,
+                type=dsc.category
+            ),
+            stages=[BlockProcStages.LOAD],
+            repr=dsc.preview,
+        )
+        t_block.save()
+        t_block.content.connect(DObject.make(dat))
+
+        loc = dict(
+            loc_page=dat.loc_page,
+            loc_bind=dat.loc_bind,
+            loc_type="page"
+        )
+        t_block.document.connect(doc, loc)
+
+    doc.refresh()
+    doc.stages.append(DocumentProcStages.IMAG)
+    doc.save()
+
+
+def make_block_kg(blk: DBlock) -> nx.DiGraph:
+    if kg := blk.kgg.get_or_none(): return kg
+
+    logger.info(f"KG ex: block {type(blk).__name__} '{blk.title[:25]}...'")
+
+    if isinstance(blk, DTxtBlock) or isinstance(blk, DImgBlock):
+        doc_input = str(blk.own_context)
+    elif isinstance(blk, DTblBlock):
+        t_ddoc: DoclingDocument = blk.content.get().content
+        doc_input = t_ddoc.export_to_markdown()
+    # elif isinstance(blk, DExcelBlock):
+    #     pass
+    else:
+        raise Exception("unknown block type")
+
+    external_context = blk.document.get().repr
+
+    ex_ecls = [i.uid for i in KType.select()]
+    ex_fcls = [i.uid for i in KFactType.select()]
+
+    kg = doc_extract_kg(doc_input, external_context, ex_ecls, ex_fcls)
+
+    blk.refresh()
+    blk.stages.append(BlockProcStages.NXKG)
+    blk.kgg.connect(DObject.make(kg))
+    blk.save()
+
+    logger.info(f"KG ex done: block {blk.title[:25]}...")
+    return kg
+
+
+def ingest_entities_blocking(blk: DBlock):
+    if BlockProcStages.KGIE in blk.stages: return
+    entity_dedup_ingest(blk)
+    blk.refresh()
+    blk.stages.append(BlockProcStages.KGIE)
+    blk.save()
+
+
+def ingest_facts(blk: DBlock):
+    if BlockProcStages.KGIR in blk.stages: return
+    fact_ingest(blk)
+    blk.refresh()
+    blk.stages.append(BlockProcStages.KGIR)
+    blk.save()
+
+
+def embed_all_blocking():
+    # to embed: DDocument, DBlock, DChunk, KFact;  skipped: KEntity
+    emb_srv = EmbeddingService()
+    to_embed = [i for i in DEmbeddable.select(repr_embedding__isnull=True) if i.repr]
+    logger.info(f"Embedding {len(to_embed)} documents")
+    embeddings = emb_srv.embed_all([i.repr for i in to_embed])
+    for n, emb in zip(to_embed, embeddings):
+        n.refresh()
+        n.repr_embedding = emb
+        n.save()
+    logger.info("Embedding done")
+
+
+def ingest_docs(doc_paths: List[Path], clean=False):
+    if clean:
+        n_setup()
+        # n_cls(all=True)
+
+    logger.info("Starting ingest documents")
+    docs = []
+    for i in doc_paths:
+        d = DocumentFile(
+            name=i.name,
+            content=i.read_bytes()
+        )
+        k = load_document(d)
+        docs.append(k)
+    logger.info("Documents created")
+
+    logger.info("Starting docling processing")
+    docs2 = []
+    for i in docs:
+        d = load_docling(i)
+        if d: docs2.append(i)
+    logger.info("Docling processing done")
+
+    logger.info("Starting loading blocks")
+    for i in docs2:
+        load_textual(i)
+        load_tables(i)
+        load_visual(i)
+    logger.info("Loading blocks done")
+
+    logger.info("Starting KG extraction")
+    for i in tqdm(list(DBlock.iter())):
+        make_block_kg(i)
+    logger.info("KG extraction done")
+
+    logger.info("Starting KG entity ingestion")
+    for i in tqdm(list(DBlock.iter())):
+        ingest_entities_blocking(i)
+    logger.info("KG entity ingestion done")
+
+    logger.info("Starting KG fact ingestion")
+    for i in tqdm(list(DBlock.iter())):
+        ingest_facts(i)
+    logger.info("KG fact ingestion done")
+
+    embed_all_blocking()
+
+# from utils.file import rd
+# from validate.ragdataset import KGRAGDataset, KGRAGDatasetBlock
+# dataset: KGRAGDataset = KGRAGDataset.model_validate_json(rd("/home/petr/study/diploma/src/workspace/extract/full/val/dataset/data.json"))
+# current_block: KGRAGDatasetBlock = dataset.document_blocks[1]
+# print(len(current_block.questions))
+# current_files = [Path("/home/petr/study/diploma/src/workspace/extract/full/val") / Path(i) for i in current_block.document_names]
+# print(current_files)
+# ingest_docs(current_files, False)
